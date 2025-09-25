38852d19e96f756977c9270ae2a286b8d9084c03
Psevdon1m <Valentine.oleynik@gmail.com>
2025-09-25 12:07:33 +0300

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "hsl(217,28%,15%)",
+        darkBlue1: "hsl(218,28%,13%)",
+        darkBlue2: "hsl(216,53%,9%)",
+        darkBlue3: "hsl(219,30%,18%)",
+        accentCyan: "hsl(176,68%,64%)",
+        accentBlue: "hsl(198,60%,50%)",
+        lightRed: "hsl(0,100%,63%)",
+      },
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      backgroundImage: (thene) => ({
+        "logo-dark-mode": "url('../images/logo.svg')",
+        "logo-light-mode": "url('../images/logo.svg')",
+        "curvy-dark-mode": "url('../images/bg-curvy-mobile.svg')",
+        "curvy-dark-mode": "url('../images/bg-curvy-mobile.svg')",
+      }),
+    },
+  },
+  varians: {
+    extend: {
+      backgroundImage: ["dark"],
+    },
+  },
+  plugins: [],
+};
